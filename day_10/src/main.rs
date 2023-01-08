844e691995a761683cfb1db5c3dd033f8b76243a
Stuart Payne <paynestu@gmail.com>
2023-01-08 06:12:34 +0000

    day_10 part two

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -6,27 +6,42 @@ use std::{
 const FILE_INPUT: &str = "input.txt";
 const CYCLE_START: i32  = 20;
 const CYCLE_STEP: i32 = 40;
+const LIT: char = '#';
+const UNLIT: char = '.';
 
 fn main() {
-    println!("{}", calc_signal(FILE_INPUT));
+    println!("{}", calc_signal(FILE_INPUT, CYCLE_STEP, CYCLE_START));
 }
 
-fn calc_signal(input: &str) -> i32 {
+fn calc_signal(input: &str, cycle_step: i32, cycle_start: i32) -> i32 {
     let mut current_cycle: i32 = 0;
     let mut current_strength: i32 = 1;
     let mut acc_strength: i32 = 0;
+    let mut sprite_starting_position: i32 = 1;
     BufReader::new(File::open(input).expect("OI BRUV YOU MAD OR WUT"))
         .lines()
         .for_each(|l| {
             let (cycles, strength) = parse_instruction(l.unwrap());
             for _ in 0..cycles {
                 current_cycle += 1;
-                if current_cycle % CYCLE_STEP == CYCLE_START {
-                    println!("{}", current_cycle);
+                let mut pixel_on_line = current_cycle % cycle_step;
+                if pixel_on_line == 0 {
+                    pixel_on_line = cycle_step;
+                }
+                if (sprite_starting_position..=sprite_starting_position + 2).contains(&pixel_on_line) {
+                    print!("{}", LIT);
+                } else {
+                    print!("{}", UNLIT);
+                }
+                if pixel_on_line == cycle_step {
+                    print!("\n");
+                }
+                if current_cycle % cycle_step == cycle_start {
                     acc_strength += current_cycle * current_strength;
                 }
             }
             current_strength += strength;
+            sprite_starting_position += strength;
             
         });
     acc_strength
@@ -48,7 +63,7 @@ mod test {
 
     #[test]
     fn part_one() {
-        assert_eq!(calc_signal("./test_input.txt"), 13140);
+        assert_eq!(calc_signal("./test_input.txt", 40, 20), 13140);
     }
 }
 
